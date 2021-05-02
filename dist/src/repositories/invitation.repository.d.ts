0da6a2412e5592436e1deae2dd37d9b5ddd2e0cc
David Asher <david.asher@fyber.com>
2021-05-03 02:35:25 +0300

    build

--- dist/src/repositories/invitation.repository.d.ts
+++ dist/src/repositories/invitation.repository.d.ts
@@ -2,22 +2,11 @@ import { Client } from '../core/client';
 import { Invitation } from '../entities/invitation.entity';
 import { InvitationScroller } from '../scrollers';
 export declare class InvitationRepository {
-    private client;
-    constructor({ client }: {
-        client: Client;
-    });
-    getSentInvitations({ skip, limit }?: {
-        skip?: number | undefined;
-        limit?: number | undefined;
-    }): InvitationScroller;
-    getReceivedInvitations({ skip, limit }?: {
-        skip?: number | undefined;
-        limit?: number | undefined;
-    }): InvitationScroller;
-    sendInvitation({ profileId, trackingId }: {
-        profileId: string;
-        trackingId: string;
-    }): Promise<Invitation>;
-    private fetchReceived;
-    private fetchSent;
+  private client;
+  constructor({ client }: { client: Client });
+  getSentInvitations({ skip, limit }?: { skip?: number | undefined; limit?: number | undefined }): InvitationScroller;
+  getReceivedInvitations({ skip, limit }?: { skip?: number | undefined; limit?: number | undefined }): InvitationScroller;
+  sendInvitation({ profileId, trackingId, msg }: { profileId: string; trackingId: string; msg?: string }): Promise<Invitation>;
+  private fetchReceived;
+  private fetchSent;
 }
