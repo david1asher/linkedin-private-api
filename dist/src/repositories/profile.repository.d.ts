6399ad22cbf0c6b043cc85d3d421d13303bf429e
David Asher <david.asher@fyber.com>
2021-06-15 03:14:48 +0300

    build

--- dist/src/repositories/profile.repository.d.ts
+++ dist/src/repositories/profile.repository.d.ts
@@ -2,18 +2,22 @@ import { Client } from '../core/client';
 import { LinkedInMiniProfile } from '../entities/linkedin-mini-profile.entity';
 import { MiniProfile, ProfileId } from '../entities/mini-profile.entity';
 import { Profile } from '../entities/profile.entity';
-export declare const getProfilesFromResponse: <T extends {
-    included: (LinkedInMiniProfile | {
-        $type: string;
-    })[];
-}>(response: T) => Record<ProfileId, MiniProfile>;
+export declare const getProfilesFromResponse: <
+  T extends {
+    data: any;
+    included: (
+      | LinkedInMiniProfile
+      | {
+          $type: string;
+        }
+    )[];
+  }
+>(
+  response: T,
+) => Record<ProfileId, MiniProfile>;
 export declare class ProfileRepository {
-    private client;
-    constructor({ client }: {
-        client: Client;
-    });
-    getProfile({ publicIdentifier }: {
-        publicIdentifier: string;
-    }): Promise<Profile>;
-    getOwnProfile(): Promise<Profile | null>;
+  private client;
+  constructor({ client }: { client: Client });
+  getProfile({ publicIdentifier }: { publicIdentifier: string }): Promise<Profile>;
+  getOwnProfile(): Promise<Profile | null>;
 }
--- dist/src/repositories/profile.repository.js
+++ dist/src/repositories/profile.repository.js
@@ -1,50 +1,64 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.ProfileRepository = exports.getProfilesFromResponse = void 0;
-const lodash_1 = require("lodash");
-const linkedin_company_entity_1 = require("../entities/linkedin-company.entity");
-const linkedin_mini_profile_entity_1 = require("../entities/linkedin-mini-profile.entity");
-const linkedin_profile_entity_1 = require("../entities/linkedin-profile.entity");
-const getProfilePictureUrls = (picture) => {
-    return lodash_1.map(picture === null || picture === void 0 ? void 0 : picture.artifacts, artifact => `${picture === null || picture === void 0 ? void 0 : picture.rootUrl}${artifact.fileIdentifyingUrlPathSegment}`);
+const lodash_1 = require('lodash');
+const linkedin_company_entity_1 = require('../entities/linkedin-company.entity');
+const linkedin_mini_profile_entity_1 = require('../entities/linkedin-mini-profile.entity');
+const linkedin_profile_entity_1 = require('../entities/linkedin-profile.entity');
+const getProfilePictureUrls = picture => {
+  return lodash_1.map(
+    picture === null || picture === void 0 ? void 0 : picture.artifacts,
+    artifact => `${picture === null || picture === void 0 ? void 0 : picture.rootUrl}${artifact.fileIdentifyingUrlPathSegment}`,
+  );
 };
-const transformMiniProfile = (miniProfile) => {
-    return {
-        ...miniProfile,
-        pictureUrls: getProfilePictureUrls(miniProfile.picture),
-        profileId: (miniProfile.entityUrn || '').replace('urn:li:fs_miniProfile:', ''),
-    };
+const transformMiniProfile = miniProfile => {
+  return {
+    ...miniProfile,
+    pictureUrls: getProfilePictureUrls(miniProfile.picture),
+    profileId: (miniProfile.entityUrn || '').replace('urn:li:fs_miniProfile:', ''),
+  };
 };
-exports.getProfilesFromResponse = (response) => {
-    const miniProfiles = lodash_1.filter(response.included, p => p.$type === linkedin_mini_profile_entity_1.MINI_PROFILE_TYPE);
-    const transformedMiniProfiles = miniProfiles.map((miniProfile) => transformMiniProfile(miniProfile));
-    return lodash_1.keyBy(transformedMiniProfiles, 'profileId');
+exports.getProfilesFromResponse = response => {
+  const miniProfiles = lodash_1.filter(response.included, p => p.$type === linkedin_mini_profile_entity_1.MINI_PROFILE_TYPE);
+  const transformedMiniProfiles = miniProfiles.map(miniProfile => transformMiniProfile(miniProfile));
+  const newProfiles = transformedMiniProfiles.map(prof => ({
+    ...prof,
+    memberDistance: response.data.elements
+      .find(a => a.type === 'SEARCH_HITS')
+      .elements.find(a => a.publicIdentifier === prof.publicIdentifier).memberDistance,
+  }));
+  return lodash_1.keyBy(newProfiles, 'profileId');
 };
 class ProfileRepository {
-    constructor({ client }) {
-        this.client = client;
+  constructor({ client }) {
+    this.client = client;
+  }
+  async getProfile({ publicIdentifier }) {
+    const response = await this.client.request.profile.getProfile({ publicIdentifier });
+    const results = response.included || [];
+    const profile = results.find(
+      r => r.$type === linkedin_profile_entity_1.PROFILE_TYPE && r.publicIdentifier === publicIdentifier,
+    );
+    const company = results.find(r => r.$type === linkedin_company_entity_1.COMPANY_TYPE && profile.headline.includes(r.name));
+    const pictureUrls = getProfilePictureUrls(lodash_1.get(profile, 'profilePicture.displayImageReference.vectorImage', {}));
+    return {
+      ...profile,
+      company,
+      pictureUrls,
+    };
+  }
+  async getOwnProfile() {
+    var _a;
+    const response = await this.client.request.profile.getOwnProfile();
+    const miniProfile =
+      (_a = response === null || response === void 0 ? void 0 : response.included) === null || _a === void 0
+        ? void 0
+        : _a.find(r => r.$type === linkedin_mini_profile_entity_1.MINI_PROFILE_TYPE);
+    if (!miniProfile) {
+      return null;
     }
-    async getProfile({ publicIdentifier }) {
-        const response = await this.client.request.profile.getProfile({ publicIdentifier });
-        const results = response.included || [];
-        const profile = results.find(r => r.$type === linkedin_profile_entity_1.PROFILE_TYPE && r.publicIdentifier === publicIdentifier);
-        const company = results.find(r => r.$type === linkedin_company_entity_1.COMPANY_TYPE && profile.headline.includes(r.name));
-        const pictureUrls = getProfilePictureUrls(lodash_1.get(profile, 'profilePicture.displayImageReference.vectorImage', {}));
-        return {
-            ...profile,
-            company,
-            pictureUrls,
-        };
-    }
-    async getOwnProfile() {
-        var _a;
-        const response = await this.client.request.profile.getOwnProfile();
-        const miniProfile = (_a = response === null || response === void 0 ? void 0 : response.included) === null || _a === void 0 ? void 0 : _a.find(r => r.$type === linkedin_mini_profile_entity_1.MINI_PROFILE_TYPE);
-        if (!miniProfile) {
-            return null;
-        }
-        return this.getProfile(miniProfile);
-    }
+    return this.getProfile(miniProfile);
+  }
 }
 exports.ProfileRepository = ProfileRepository;
 //# sourceMappingURL=profile.repository.js.map
